79854105eb39f041f4cc55b964f2e67715456ec7
rishabhkashyap01 <rishabhkashyap8135@gmail.com>
2026-03-13 10:21:19 +0530

    minor middleware name issue

--- frontend/proxy.ts
+++ frontend/proxy.ts
@@ -2,7 +2,7 @@ import { NextRequest, NextResponse } from "next/server";
 
 const PUBLIC_PATHS = ["/login"];
 
-export function middleware(req: NextRequest) {
+export function proxy(req: NextRequest) {
   const { pathname } = req.nextUrl;
 
   // Allow public paths
